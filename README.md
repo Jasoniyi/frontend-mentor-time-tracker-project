43b3e5ba78cb23438b1712bd34e80fb11b3202b4
unknown <jasoniyi>
2022-02-16 14:52:06 +0100

    added js

---
+++ README.md
@@ -0,0 +1,16 @@
+This is a solution to the [Time tracking dashboard challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/time-tracking-dashboard-UIQ7167Jw). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](# Frontend Mentor - Time tracking dashboard )
+  - [Screenshot](#screenshot)
+  - [Links](https://modest-wright-514ee9.netlify.app)
+- [My process](#my-process)
+  - [Built with]
+    HTML, css
+  - [What I learned](#what-i-learned - i learned how to use grids to arrange elements)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources - youtube and goggle)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
---
+++ index.js
@@ -0,0 +1,78 @@
+// const day = document.getElementById('day');
+const day = document.querySelector('#day');
+const week = document.querySelector('#week');
+const month = document.querySelector('#month');
+
+
+const workHr = document.getElementById('workHr');
+const workWk = document.getElementById('workWk');
+const playHr = document.querySelector('.playHr');
+const playWk = document.querySelector('.playWk');
+const studyHr = document.querySelector('.studyHr');
+const studyWk = document.querySelector('.studyWk');
+const exeHr = document.querySelector('.exeHr');
+const exeWk = document.querySelector('.exeWk');
+const socialHr = document.querySelector('.socialHr');
+const socialWk = document.querySelector('.socialWk');
+const selfHr = document.querySelector('.selfHr');
+const selfWk = document.querySelector('.selfWk');
+
+
+
+
+
+
+
+day.addEventListener('click', () => {
+     day.classList.add('add-active');
+     week.classList.remove('add-active');
+     month.classList.remove('add-active');
+    workHr.textContent = '5Hrs';
+    workWk.textContent = 'Last week - 7hrs';
+    playHr.textContent = '1Hrs'
+    playWk.textContent = 'Last week - 2hrs';
+    studyHr.textContent = '0Hrs'
+    studyWk.textContent = 'Last week - 1hrs';
+    exeHr.textContent = '1'
+    exeWk.textContent = 'Last week - 1hrs';
+    socialHr.textContent = '1Hrs'
+    socialWk.textContent = 'Last week - 3hrs';
+    selfHr.textContent = '0Hrs'
+    selfWk.textContent = 'Last week - 1hrs';
+});
+
+week.addEventListener('click', () => {
+    week.classList.add('add-active');
+    day.classList.remove('add-active');
+    month.classList.remove('add-active');
+   workHr.textContent = '32Hrs';
+   workWk.textContent = 'Last week - 36hrs';
+   playHr.textContent = '10Hrs'
+   playWk.textContent = 'Last week - 8hrs';
+   studyHr.textContent = '4Hrs'
+   studyWk.textContent = 'Last week - 7hrs';
+   exeHr.textContent = '4Hrs'
+   exeWk.textContent = 'Last week - 5hrs';
+   socialHr.textContent = '5Hrs' 
+   socialWk.textContent = 'Last week - 10hrs';
+   selfHr.textContent = '2Hrs'
+   selfWk.textContent ='Last week - 2hrs';
+})
+
+month.addEventListener('click', () => {
+    month.classList.add('add-active');
+    week.classList.remove('add-active');
+    day.classList.remove('add-active');
+   workHr.textContent = '103Hrs';
+   workWk.textContent = 'Last week - 128hrs';
+   playHr.textContent = '13Hrs'
+   playWk.textContent = 'Last week - 29hrs';
+   studyHr.textContent = '13Hrs'
+   studyWk.textContent = 'Last week - 19hrs';
+   exeHr.textContent = '11Hrs'
+   exeWk.textContent = 'Last week - 18hrs';
+   socialHr.textContent = '21Hrs' 
+   socialWk.textContent = 'Last week - 23hrs';
+   selfHr.textContent = '7Hrs'
+   selfWk.textContent ='Last week - 11hrs';
+})
